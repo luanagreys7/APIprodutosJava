daab2ccad3ac343c36aed76dec9b8a2bba40c898
luanagreys7 <luanagreys7@gmail.com>
2025-10-29 11:15:04 -0300

    Conteúdo controller / ProdutoController.java

--- src/main/java/com/example/api_produtos/controller/ProdutoController.java
+++ src/main/java/com/example/api_produtos/controller/ProdutoController.java
@@ -17,9 +17,40 @@ import com.example.api_produtos.repository.ProdutoRepository;
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+
     private final ProdutoRepository repository;
 
     public ProdutoController(ProdutoRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto) {
+        return repository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscar(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, 
+                        @RequestBody Produto novoProduto) {
+        return repository.findById(id).map(produto -> {
+            produto.setNome(novoProduto.getNome());
+            produto.setPreco(novoProduto.getPreco());
+            return repository.save(produto);
+        }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
